3689ba28ae69ba002e9ca6354ef88fd857fee301
LeafLight <841789075@qq.com>
2025-10-13 15:08:39 +0800

    FT

--- UpdateDoc.R
+++ UpdateDoc.R
@@ -1,10 +1,11 @@
 #!/usr/bin/env Rscript
 # UpdateDoc.R —— 一键更新文档 & 本地检查
-pkg_path <- dirname(rstudioapi::getActiveDocumentContext()$path)   # 文件所在文件夹
+pkg_path <- dirname('./')   # 文件所在文件夹
 if (!file.exists(file.path(pkg_path, "DESCRIPTION")))
   stop("DESCRIPTION not found! 请把本文件放在包根目录。")
 
 setwd(pkg_path)                          # 确保工作目录正确
 devtools::document()                     # 生成 Rd + NAMESPACE
-devtools::check()                        # 本地 R CMD check
-message("✅ 文档已更新，检查完成！")
+#devtools::check()                        # 本地 R CMD check
+#message("✅ 文档已更新，检查完成！")
+message("✅ 文档已更新！")
